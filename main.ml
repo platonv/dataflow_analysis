b0eb71a5b948f31fccc93ba395537a949ab7e0e3
alexandraneamtu <alexandra.neamtu@iquate.com>
2019-05-20 15:11:23 +0300

    Added backward dataflow analysis for live variables

--- main.ml
+++ main.ml
@@ -24,6 +24,9 @@ and nodeList = node list
 and pred = stmt list
 and succ = stmt list
 and cfg = CFG of nodeList
+and inN = var list
+and outN = var list
+and dataflowNode = DataFlowNode of node * inN * outN
 ;;
 
 let rec string_of_var = function
@@ -122,9 +125,41 @@ let rec string_of_cfg (cfg: nodeList) =
     | Node(stmt, pred, succ) :: tail -> (string_of_pred pred) ^ (string_of_stmt stmt) ^ (string_of_succ succ) ^ (string_of_cfg tail)
   ;;
 
+
+let rec string_of_var_list (use_vars: var list) =
+  match use_vars with
+    | [] -> ""
+    | v :: tail -> "~~~~" ^ (string_of_var v) ^ "\n" ^ string_of_var_list tail
+  ;;
+
+let in_of_node = function
+  | DataFlowNode(_, i, _) -> i
+  ;;
+
+let out_of_node = function
+  | DataFlowNode(_, _, out) -> out
+  ;;
+
+let node_of_dfNode = function
+  | DataFlowNode(n, _, _) -> n
+  ;;
+
 let stmt_of = function
   | Node(s, _, _) -> s
   ;;
+
+let rec string_of_dataflow_nodes (nodes: dataflowNode list) =
+  let s = List.map (fun n ->
+    (string_of_stmt (stmt_of (node_of_dfNode n))) ^ "\nIN:\n" ^ string_of_var_list (in_of_node n) ^ "\nOUT:\n" ^ string_of_var_list (out_of_node n)
+  ) nodes in
+  String.concat "\n" s
+  ;;
+
+let succ_of = function
+  | Node(_, _, [NullStmt]) -> []
+  | Node(_, _, s) -> s
+  ;;
+
 
 exception Error;;
 
@@ -136,6 +171,16 @@ let rec node_of_statement (s: stmt) (nodes: nodeList) =
                                       n
                                       else
                                       node_of_statement s tail
+  ;;
+
+let rec dataflowNode_of_node (n: node) (dfNodes: dataflowNode list)=
+  match dfNodes with
+    | [] -> raise Error
+    | h :: tail -> match h with
+                    | DataFlowNode(nn, _, _) -> if n = nn then 
+                                                h
+                                              else
+                                              dataflowNode_of_node n tail
   ;;
 
 let rec cfg_from_ast (currStmt: stmt) (nodes: nodeList) (postStmt: stmt) =
@@ -171,10 +216,95 @@ let rec proc (nodes: nodeList) =
   List.map (function | Node(st, _, s) -> Node(st, (find_predecessors st nodes), s)) nodes
   ;;
 
+let rec hasIdArithExp = function
+  | Add (exp1, exp2) -> hasId(exp1) @ hasId(exp2)
+  | Sub (exp1, exp2) -> hasId(exp1) @ hasId(exp2)
+  | Mul (exp1, exp2) -> hasId(exp1) @ hasId(exp2)
+  | Div (exp1, exp2) -> hasId(exp1) @ hasId(exp2)
+
+and hasId = function
+  | Number (_) -> []
+  | Id (v) -> [v]
+  | ArithExp (exp) -> hasIdArithExp(exp)
+;;
+
+
+
+let gen (currentNode: node) (defList: var list) =
+  let stmt = stmt_of currentNode in
+  match stmt with
+    | AssignStmt (var, exp) -> defList @ [var]
+    | _ -> defList ;;
+
+let use (currentNode: node)  =
+  let stmt = stmt_of currentNode in
+  match stmt with
+    | AssignStmt (_, exp) ->  hasId exp
+    | IfStmt (exp, _, _) -> hasId exp
+    | PrintStmt (exp) -> hasId exp
+    | WhileStmt (exp, stmt) -> hasId exp;;
+
+let print_def (nodes: nodeList) =
+  let s = List.map (function | Node(st, p, s) -> (string_of_stmt st)   ^ "GEN:\n" ^ (string_of_var_list (gen (Node(st,p,s)) []))) nodes in
+  String.concat "\n" s
+;;
+
+let print_use (nodes: nodeList) =
+  let s = List.map (function | Node(st, p, s) -> (string_of_stmt st)   ^ "USE:\n" ^ (string_of_var_list (use (Node(st,p,s))))) nodes in
+  String.concat "\n"  s
+;;
+
 
 let cfg = match p with
   | Program(stmt) -> cfg_from_ast stmt [] NullStmt
   ;;
+
+ let rev list =
+  let rec aux acc = function
+    | [] -> acc
+    | h::t -> aux (h::acc) t in
+  aux [] list
+  ;;
+
+let diff l1 l2 = List.filter (fun x -> not (List.mem x l2)) l1
+;;
+
+
+let compute_in (node: node) (outList: outN) =
+  let defN = gen node [] in
+  let useN = use node in
+  let res = useN @ (diff outList defN) in
+  List.sort_uniq (fun x y -> compare x y) res
+;;
+
+
+let compute_out (node: node) (nodes: nodeList) (dfNList: dataflowNode list) =
+    let succ = succ_of node in 
+    let nodes_succ = List.map (fun suc -> node_of_statement suc nodes) succ in
+    let res = List.flatten (List.map (fun succ -> (in_of_node (dataflowNode_of_node succ dfNList))) nodes_succ) in
+    List.sort_uniq (fun x y -> compare x y) res
+  ;;
+
+
+let rec compute_rec_dataFlow (remaining: nodeList) (allNodes: nodeList) (dfNList: dataflowNode list) =
+  match remaining with
+    | [] -> []
+    | n :: tl ->  let outVars = compute_out n allNodes dfNList in
+                  let newNode = DataFlowNode (n, (compute_in n outVars), compute_out n allNodes dfNList) in
+                   [newNode]  @ compute_rec_dataFlow (tl) (allNodes) ([newNode] @ dfNList)
+;;
+
+let compute_initial_dataFlow (nodes: nodeList) = 
+  let backward_nodes = rev nodes in
+  if List.length (backward_nodes) > 0 then
+    let n = List.hd (backward_nodes) in
+    let tl = List.tl (backward_nodes) in
+    let first_dataflow_node = DataFlowNode (n, (compute_in n []), []) in
+     [first_dataflow_node] @  compute_rec_dataFlow (tl) (nodes) ([first_dataflow_node]) 
+  else
+  []
+;;
+
 
 print_string "Program: \n" ;;
 print_string "----------------------\n" ;;
@@ -184,4 +314,22 @@ print_string "----------------------\n" ;;
 print_string "CFG: \n" ;;
 print_string "----------------------\n" ;;
 print_string (string_of_cfg (proc cfg)) ;;
+print_string "----------------------\n" ;;
+
+let cfg = proc cfg;;
+
+print_string "GEN: \n";;
+print_string "----------------------\n" ;;
+print_string (print_def (proc cfg)) ;;
+print_string "----------------------\n" ;;
+
+
+print_string "USE: \n";;
+print_string "----------------------\n" ;;
+print_string (print_use (proc cfg)) ;;
+print_string "----------------------\n" ;;
+
+print_string "DF: \n";;
+print_string "----------------------\n" ;;
+print_string (string_of_dataflow_nodes (compute_initial_dataFlow cfg)) ;;
 print_string "----------------------\n" ;;
